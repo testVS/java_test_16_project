7107bbcf095369760c5f199b30d529606d71968f
testVS <55035900+testVS@users.noreply.github.com>
2020-03-15 18:00:57 +0200

    Лекция 1.7 Функции
    - Добавил пару функции

--- sandbox/src/Main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/Main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,22 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
+		double a = 5.5;
+		double b = 6.5;
 		hello("MOON15");
+		System.out.println("Square are is: " + area(a));
+		System.out.println("Rectangle are is: " + area(a,b));
  }
 	public static void hello(String somebody) {
 		System.out.println("Hello" + somebody + "!") ;
+		;
 	}
 
+	public static double area (Double a){
+		return a * a;
+	}
+
+	public static double area (Double a, Double b){
+		return a * b;
+	}
 }
