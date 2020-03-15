5544b987a3a1e349be3e052fa3581b82bf8b3fac
testVS <55035900+testVS@users.noreply.github.com>
2020-03-15 17:47:45 +0200

    добавил шаг Build в конфиг для рекомпиляции перед запуском

--- sandbox/src/Main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/Main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello MOON") ;
+		hello("MOON15");
  }
+	public static void hello(String somebody) {
+		System.out.println("Hello" + somebody + "!") ;
+	}
+
 }
