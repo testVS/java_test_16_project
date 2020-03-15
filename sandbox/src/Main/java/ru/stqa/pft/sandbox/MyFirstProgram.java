60f2096a76875342e15f5fd5f7e74954c004d7b4
testVS <55035900+testVS@users.noreply.github.com>
2020-03-15 22:05:37 +0200

    перевел функции square и rectangle в соответсвующие классы

--- sandbox/src/Main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/Main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,21 +5,16 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		hello(" MOON15");
 
-		Square s = new Square(5);
-		System.out.println("Square area is: " + area(s));
+		Square s = new Square(4);
+		System.out.println("Square area is: " + s.area());
 
-		Rectangle r = new Rectangle(10,5.1);
-		System.out.println("Rectangle area is: " + area(r));
+		Rectangle r = new Rectangle(11,7.1);
+		System.out.println("Rectangle area is: " + r.area());
  }
 	public static void hello(String somebody) {
 		System.out.println("Hello" + somebody + " !") ;
 	}
 
-	public static double area (Square s){
-		return s.l * s.l;
-	}
 
-	public static double area (Rectangle r){
-		return r.a * r.b;
-	}
+
 }
--- sandbox/src/Main/java/ru/stqa/pft/sandbox/Rectangle.java
+++ sandbox/src/Main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -9,4 +9,7 @@ public class Rectangle {
         this.b = b;
     }
 
+    public double area (){
+        return this.a * this.b;
+    }
 }
