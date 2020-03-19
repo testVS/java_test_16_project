d0ccd0c0cf8364376fdf9bafe091bcad8115c11f
testVS <55035900+testVS@users.noreply.github.com>
2020-03-19 04:06:14 +0200

    Переделал домашку с классом Point
    - создал класс Point
    - в классе Point создал конструктор
    - в классе Point создал функцию вычесления координат
    - в вызываемом классе создал 2 обьекта
    - в вызываемом классе создал метод считывающий растояние между точками
    - в вызываемом классе создал метод распечатывающий растояние между точками

--- sandbox/src/Main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/Main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		hello(" MOON15");
+		hello(" World, this is my UPDATED homework #2 ");
 
 		Square s = new Square(4);
 		System.out.println("Square area is: " + s.area());
@@ -11,10 +11,16 @@ public class MyFirstProgram {
 		Rectangle r = new Rectangle(11, 7.1);
 		System.out.println("Rectangle area is: " + r.area());
 
-
+		Point p1 = new Point(5,4);
+		Point p2 = new Point(5,4);
+		System.out.println("Растояние между p1 и p2 = " + distance(p1,p2) );
 	}
 
 	public static void hello(String somebody) {
-		System.out.println("Hello" + somebody + " !");
+		System.out.println("Hello Alexei" + somebody + " !");
+	}
+
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt(p1.coord() + p2.coord());
 	}
 }
---
+++ sandbox/src/Main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public  Point (double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double coord(){
+        return x * y;
+    }
+}
